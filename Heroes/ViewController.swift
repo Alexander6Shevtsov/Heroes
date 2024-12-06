b8c8877b497bf9c83aa9528bc45d9fc8d431443d
Alexander6Shevtsov <axrsvr@gmail.com>
2024-12-06 10:26:49 +0400

    Initial Commit

---
+++ Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Heroes
+//
+//  Created by Alexander Shevtsov on 06.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
