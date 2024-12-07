804c8f986a45206c4c323b24139025a90905a9cd
Alexander6Shevtsov <axrsvr@gmail.com>
2024-12-07 16:54:39 +0400

    Add Kingfisher

---
+++ Heroes/Models/Superhero.swift
@@ -0,0 +1,8 @@
+//
+//  Superhero.swift
+//  Heroes
+//
+//  Created by Alexander Shevtsov on 06.12.2024.
+//
+
+import Foundation
---
+++ Heroes/Views/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Heroes
+//
+//  Created by Alexander Shevtsov on 06.12.2024.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
